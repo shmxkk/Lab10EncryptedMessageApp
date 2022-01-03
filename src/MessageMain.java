282211d2780bc3f5c3f1f640daa0c1a3212bf184
Jessica Bahny <77072076+silversun307@users.noreply.github.com>
2022-01-03 15:27:36 -0700

    Delete MessageMain.java

--- src/MessageMain.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class MessageMain {
-
-    ArrayList<Message> messages = new ArrayList<>();
-    //build an array list of messages and do stuff..do we provide or have them code the main?
-}
